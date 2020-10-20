6eb1f6531b844ce65cb30868d906b74d4e96e521
qjimmy <quang-jimmy@hotmail.com>
2020-10-20 03:13:54 -0400

    Base MLP with some custom parameters

---
+++ Base-MLP/BaseMLP.py
@@ -0,0 +1,14 @@
+import pandas as pd
+from sklearn.neural_network import MLPClassifier
+
+def base_MLP_model(training_file):
+  df = pd.read_csv(training_file, header=None)
+  labels = df[1024]
+
+  del df[1024]
+  features = [feature for feature in df.to_numpy()]
+
+  model = MLPClassifier(activation='logistic', hidden_layer_sizes=(100,), solver='sgd',)
+  model.fit(features, labels)
+
+  return model
